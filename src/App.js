efa3f1cf1c8d36e592edadc0768db0fa812cc26d
ganesh nikale <ganesh.nikale@digitas.com>
2022-09-01 17:35:04 +0530

    react test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let innitialState = [
+    {
+      name: "emp1",
+      salary: 1000,
+      designation: "Developer"
+    }, {
+      name: "emp2",
+      salary: 2000,
+      designation: "Tester"
+    }, {
+      name: "emp3",
+      salary: 3000,
+      designation: "Manager"
+    }, {
+      name: "emp4",
+      salary: 1000,
+      designation: "Developer"
+    }
+    ];
+
+
+    const [value, setValue] = useState(innitialState);
+
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<table>
+<thead>
+  <tr>
+    <th>Name</th>
+    <th>Salary</th>
+    <th>Designation</th>
+  </tr>
+</thead>
+      {
+        value.map((emp, index) => {
+          return (<tr key={index}>
+            <td>{emp.name}</td>
+            <td>{emp.salary}</td>
+            <td>{emp.designation}</td>
+          </tr>)
+        })
+      }
+</table>
+
+     
     </div>
   );
 }
